86d15641a9c4c12e84631d38b82eb322c42019a5
RushaanMahajan <c25rushaan.mahajan@afacademy.af.edu>
2025-04-08 11:34:57 -0600

    task 3 code

--- structured_outputs/user_creation.py
+++ structured_outputs/user_creation.py
@@ -1,5 +1,31 @@
-'''Passes a personal intro statement to an LLM.
-The LLM produces valid JSON that could be ingested into a database to create a new user.
+from ollama import chat
+from pydantic import BaseModel
 
-Modified from https://ollama.com/blog/structured-outputs
-'''
+
+class Pet(BaseModel):
+  name: str
+  animal: str
+  age: int
+  color: str | None
+  favorite_toy: str | None
+ 
+class PetList(BaseModel):
+  pets: list[Pet]
+ 
+response = chat(
+  messages=[
+    {
+      'role': 'user',
+      'content': '''
+        I have two pets.
+        A cat named Luna who is 5 years old and loves playing with yarn. She has grey fur.
+        I also have a 2 year old black cat named Loki who loves tennis balls.
+      ''',
+    }
+  ],
+  model='llama3.1',
+  format=PetList.model_json_schema(),
+)
+ 
+pets = PetList.model_validate_json(response.message.content)
+print(pets)
