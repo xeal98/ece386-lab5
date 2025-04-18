091d17bcc7ec99bd62bf7585dc5ca9b32f496016
xeal98 <xeal03@gmail.com>
2025-04-17 23:22:01 -0600

    Ran Black Formatter

--- structured_outputs/user_creation.py
+++ structured_outputs/user_creation.py
@@ -3,29 +3,31 @@ from pydantic import BaseModel
 
 
 class Pet(BaseModel):
-  name: str
-  animal: str
-  age: int
-  color: str | None
-  favorite_toy: str | None
- 
+    name: str
+    animal: str
+    age: int
+    color: str | None
+    favorite_toy: str | None
+
+
 class PetList(BaseModel):
-  pets: list[Pet]
- 
+    pets: list[Pet]
+
+
 response = chat(
-  messages=[
-    {
-      'role': 'user',
-      'content': '''
+    messages=[
+        {
+            "role": "user",
+            "content": """
         I have two pets.
         A cat named Luna who is 5 years old and loves playing with yarn. She has grey fur.
         I also have a 2 year old black cat named Loki who loves tennis balls.
-      ''',
-    }
-  ],
-  model='gemma3:4b',
-  format=PetList.model_json_schema(),
+      """,
+        }
+    ],
+    model="gemma3:4b",
+    format=PetList.model_json_schema(),
 )
- 
+
 pets = PetList.model_validate_json(response.message.content)
 print(pets)
